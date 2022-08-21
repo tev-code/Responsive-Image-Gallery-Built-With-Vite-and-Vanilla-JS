604699aa6c3998998a2611cfdb8d05f30e7eb62c
Tevin <you@example.com>
2022-08-21 16:32:21 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*== SWIPER JS GALLERY ==*/
+let swipeCards = new Swiper(".gallery-cards", {
+  loop: true,
+  loopedSlides: 5,
+  cssMode: true,
+  effect: 'fade',
+});
+
+let swipeThumbs = new Swiper(".gallery-thumbs", {
+  loop: true,
+  loopedSlides: 5,
+  slidesPerview: 3,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
